3de263c7a16a13571e2987003f86e217d4252bd8
Arthurdw <dev@arthurdw.com>
2024-03-01 20:06:49 +0100

    pass 2nd stage

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,36 @@
-// Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::error::Error;
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::str;
+
+fn handle_message(_message: &str) -> &str {
+    "+PONG\r\n"
+}
+
+fn handle_connetion(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    let mut data: [u8; 512] = [0; 512];
+
+    loop {
+        let n = stream.read(&mut data)?;
+
+        if n == 0 {
+            break;
+        }
+
+        let content: &str = str::from_utf8(&data[0..n])?;
+        let response = handle_message(content);
+        stream.write_all(response.as_ref())?;
+    }
+
+    Ok(())
+}
 
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
-            }
+            Ok(stream) => handle_connetion(stream).expect("Something went wrong in the connection"),
             Err(e) => {
                 println!("error: {}", e);
             }
